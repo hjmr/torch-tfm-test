e552e14740ee6976baa295ba5eda7fcbaddd5d0d
hjmr <2620990+hjmr@users.noreply.github.com>
2024-06-06 15:02:21 +0900

    tf

---
+++ tf.py
@@ -0,0 +1,31 @@
+import torch
+from torch import nn, Tensor
+from torch.nn import TransformerEncoder, TransformerEncoderLayer
+
+
+class TransformerModel(nn.Module):
+    def __init__(
+        self,
+        d_model: int,
+        nhead: int,
+        num_encoder_layers: int,
+        num_decoder_layers: int,
+        dim_feedforward: int,
+        dropout: float = 0.5,
+    ):
+        super().__init__()
+        self.model_type = "Transformer"
+        encoder_layers = TransformerEncoderLayer(d_model, nhead, dim_feedforward, dropout)
+        self.transformer_encoder = TransformerEncoder(encoder_layers, num_encoder_layers)
+        self.d_model = d_model
+        self.linear = nn.Linear(d_model, d_model)
+
+        self.init_weights()
+
+    def init_weights(self) -> None:
+        initrange = 0.1
+        self.linear.bias.data.zero_()
+        self.linear.weight.data.uniform_(-initrange, initrange)
+
+    def forward(self, src: Tensor, src_mask: Tensor = None) -> Tensor:
+        pass
